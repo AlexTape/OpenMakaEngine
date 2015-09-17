5439e98ff56da41d8a8bb619ead74f9c47c5d1b8
AlexTape <alexalone@web.de>
2015-09-17 13:04:21 +0200

    commit new testrun with 101 iterations

--- src/App.cpp
+++ src/App.cpp
@@ -80,8 +80,8 @@ int processTests() {
     Controller *controller = Controller::getInstance();
     Controller::STORAGE_PATH = path;
 
-    // do testcase 0 (all) 51 times
-    controller->test(0, 51);
+    // do testcase 0 (all) 101 times
+    controller->test(0, 101);
 
     // idle to view images
     if (Controller::MODE_USE_WINDOWS) {
